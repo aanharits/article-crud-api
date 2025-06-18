db960a0b0a16b2a84da5f7a5adb2e18353852f43
aanharits <farhan.harits05@gmail.com>
2025-06-18 15:29:26 +0700

    add: routes delete

--- src/routes/postRoutes.js
+++ src/routes/postRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const { getPosts, createPosts, updatePosts } = require('../controller/postController');
+const { getPosts, createPosts, updatePosts, deletePosts } = require('../controller/postController');
 const postRoutes = express.Router();
 
 postRoutes.get('/posts', getPosts);
 postRoutes.post('/posts', createPosts)
 postRoutes.put('/posts/:id', updatePosts);
+postRoutes.delete('/posts/:id', deletePosts);
 
 module.exports = postRoutes;
