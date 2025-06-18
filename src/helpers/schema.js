89a2eae4123c8bd5bd28daeb8b47a3843e803e5c
aanharits <farhan.harits05@gmail.com>
2025-06-18 17:05:49 +0700

    create: validation data for auth with zod

--- src/helpers/schema.js
+++ src/helpers/schema.js
@@ -7,6 +7,13 @@ const postSchema = z.object({
     published: z.boolean()
 })
 
+const userSchema = z.object({
+    email: z.string().email(),
+    password: z.string().min(5),
+    name: z.string()
+})
+
 module.exports = {
-    postSchema
+    postSchema,
+    userSchema
 }
