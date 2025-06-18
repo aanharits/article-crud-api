057fdb33a66ab2259094f5d31ae5af744e9da806
aanharits <farhan.harits05@gmail.com>
2025-06-18 15:19:24 +0700

    add: routes update

--- src/routes/postRoutes.js
+++ src/routes/postRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const { getPosts, createPosts } = require('../controller/postController');
+const { getPosts, createPosts, updatePosts } = require('../controller/postController');
 const postRoutes = express.Router();
 
 postRoutes.get('/posts', getPosts);
 postRoutes.post('/posts', createPosts)
+postRoutes.put('/posts/:id', updatePosts);
 
 module.exports = postRoutes;
