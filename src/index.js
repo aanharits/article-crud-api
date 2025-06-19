394dba3ed4bc853e2eb956af0587ddfd331efa21
aanharits <farhan.harits05@gmail.com>
2025-06-20 00:22:49 +0700

    changes

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 3000;
-const postRoutes = require("./routes/postRoutes.js");
 const authRoutes = require("./routes/authRoutes.js");
+const postRoutes = require("./routes/postRoutes.js");
 
 app.use(cors());
 app.use(express.json());
@@ -12,8 +12,8 @@ app.get("/", (req, res) => {
     res.send("API is running...");
 });
 
-app.use('/api', postRoutes);
 app.use('/api', authRoutes);
+app.use('/api', postRoutes);
 
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
