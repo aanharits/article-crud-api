5eeb46a12f77280e350e54fc6edf1966ec992e17
Farhan Harits Prakoso <farhan.harits05@gmail.com>
2025-09-25 03:01:59 +0700

    changes

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ const dotenv = require("dotenv");
 const authRoutes = require("./routes/authRoutes.js");
 const postRoutes = require("./routes/postRoutes.js");
 
+const port = process.env.PORT;
+
 dotenv.config();
 
 const app = express();
@@ -17,5 +19,9 @@ app.get("/", (req, res) => {
 
 app.use("/auth", authRoutes);
 app.use("/api", postRoutes);
+
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+});
 
 module.exports = app;
